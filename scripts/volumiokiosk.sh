23102c89c99cbb4f162281e2e69b4a4b129c235d
juju <julien@bluemind.org>
2021-04-16 18:57:35 +0200

    first comit for new version

---
+++ scripts/volumiokiosk.sh
@@ -0,0 +1,30 @@
+/usr/lib/xorg/Xorg -nolisten tcp -novtswitch -nocursor &
+
+mkdir /tmp/volumiokiosk
+
+DISPLAY=:0 /usr/bin/chromium-browser \
+--no-sandbox \
+--simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' \
+--force-device-scale-factor=1 \
+--disable-pinch \
+--kiosk \
+--no-first-run \
+--noerrdialogs \
+--disable-breakpad \
+--disable-crash-reporter \
+--disable-infobars \
+--disable-session-crashed-bubble \
+--disable-translate \
+--use-gl=egl \
+--window-size=800,480 \
+--disable-gpu-vsync \
+--disable-quic \
+--enable-fast-unload \
+--enable-checker-imaging \
+--enable-tcp-fast-open \
+--enable-native-gpu-memory-buffers \
+--enable-gpu-rasterization \
+--enable-zero-copy \
+--num-raster-threads=4 \
+--user-data-dir='/tmp/volumiokiosk' \
+http://localhost:3000
--- vu_meter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cava -p /home/volumio/.config/cava/config | /opt/leds_vumeter.sh
