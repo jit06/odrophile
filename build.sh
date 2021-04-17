6c48ff7fe4e9b8cef4d508d742fe7dc63e13eb99
volumio <juju@bluemind.org>
2021-04-17 18:04:54 +0000

    uncommented last part

--- build.sh
+++ build.sh
@@ -42,29 +42,29 @@ cp scripts/boot.ini /media/boot/
 
 
 # set default uboot hdmi mode to 640x480 (default is 720p)
-#echo "------------ set uboot splash"
-#fw_setenv hdmimode 480p
-#fw_setenv outputmode 480p
-#fw_setenv fb_width 640
-#fw_setenv fb_height 480
-#fw_setenv display_width 640
-#fw_setenv display_height 480
+echo "------------ set uboot splash"
+fw_setenv hdmimode 480p
+fw_setenv outputmode 480p
+fw_setenv fb_width 640
+fw_setenv fb_height 480
+fw_setenv display_width 640
+fw_setenv display_height 480
 
 
 # tell uboot to load the boot splash
-#cp gfx/boot-logo.bmp /media/boot/
-#fw_setenv preloadlogo 'video open;video clear; video dev open ${outputmode};fatload mmc 0:1 ${loadaddr_logo} boot-logo.bmp;bmp display ${loadaddr_logo}; bmp scale'
+cp gfx/boot-logo.bmp /media/boot/
+fw_setenv preloadlogo 'video open;video clear; video dev open ${outputmode};fatload mmc 0:1 ${loadaddr_logo} boot-logo.bmp;bmp display ${loadaddr_logo}; bmp scale'
 
 
 # handle the second splash screen in initram-fs
-#echo "------------ set system splash"
-#cp gfx/logo.lzo /etc/
-#cp scripts/cpimg.sh /etc/initramfs-tools/hooks/
+echo "------------ set system splash"
+cp gfx/logo.lzo /etc/
+cp scripts/cpimg.sh /etc/initramfs-tools/hooks/
 
-#echo "busybox lzop -d -c /etc/logo.lzo > /dev/fb0" >> /etc/initramfs-tools/scripts/local-top/c1_init.sh
+echo "busybox lzop -d -c /etc/logo.lzo > /dev/fb0" >> /etc/initramfs-tools/scripts/local-top/c1_init.sh
 
-#update-initramfs -u
-#mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n uInitrd -d /boot/initrd.img-3.10.107-13 /media/boot/uInitrd
+update-initramfs -u
+mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n uInitrd -d /boot/initrd.img-3.10.107-13 /media/boot/uInitrd
 
 
 echo "----------- finished"
